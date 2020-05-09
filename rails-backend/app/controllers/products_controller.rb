6aa04a7dc2e9491c5e9de58353f6162c056b9150
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-05-09 19:48:37 -0400

    Update products_controller.rb

--- rails-backend/app/controllers/products_controller.rb
+++ rails-backend/app/controllers/products_controller.rb
@@ -1,2 +1,37 @@
 class ProductsController < ApplicationController
+
+    def index 
+        @products = Product.all 
+        render json: @products, status:200
+    end
+
+    def show 
+        @product = Product.find(params[:id])
+    end
+
+    def create 
+        @product = Product.create(product_params)
+    end
+
+    def update 
+        @product = Product.find(params[:id])
+        @product.update(product_params)
+        render json: @product, status: 200 
+    end 
+
+    def destroy 
+        @product = Product.find(params[:id])
+        @product.delete
+        render json: {product_id: @product.id }
+    end 
+
+    private 
+        def product_params
+            params.require(:product).permit(
+                :brand, 
+                :category, 
+                :full_size, 
+                :notes
+            )
+        end
 end
