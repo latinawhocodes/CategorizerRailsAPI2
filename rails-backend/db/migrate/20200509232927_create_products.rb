85aaef15e1ee3468cd69de83534d5c008d70a90a
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-05-09 19:31:30 -0400

    Update 20200509232927_create_products.rb
    Update products table

--- rails-backend/db/migrate/20200509232927_create_products.rb
+++ rails-backend/db/migrate/20200509232927_create_products.rb
@@ -1,8 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.brand :references
-      t.category :references
+      t.string :name
+      t.boolean :fullsize
+      t.text :notes
+      t.references :brand, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
