2577ae3ca0cd90db3f9a4e776e0772e67f823dff
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-05-09 19:38:25 -0400

    Update 20200509232902_create_brands.rb

--- rails-backend/db/migrate/20200509232902_create_brands.rb
+++ rails-backend/db/migrate/20200509232902_create_brands.rb
@@ -1,7 +1,7 @@
 class CreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands do |t|
-      t.string :brand_name
+      t.string :name
       t.string :price_range
 
       t.timestamps
