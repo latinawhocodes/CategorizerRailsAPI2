bb0012031d26b6e82086ca48aac3fe1ba5d509ba
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-05-09 20:39:12 -0400

    update

--- rails-backend/app/controllers/categories_controller.rb
+++ rails-backend/app/controllers/categories_controller.rb
@@ -2,30 +2,42 @@ class CategoriesController < ApplicationController
 
     def index 
         @categories = Category.all 
-        render json: @categories, include: [:products], status:200
+        render json: @categories, include: [:products], status: 200
     end
 
     def show 
         @category = Category.find(params[:id])
+        render json: @category, status: 200 
     end
 
     def create 
         @category = Category.create(category_params)
+        if @category.save
+            render json: @category, status: 200 
+        else 
+            render json: {message: "Category not created!"}
+        end 
     end
 
     def update 
         @category = Category.find(params[:id])
-        @category.update(category_params)
-        render json: @category, status: 200 
+        if @category.update(category_params)
+            render json: @category, status: 200 
+        else 
+            render json: {message: "Category not updated!"}
+        end 
     end 
 
     def destroy 
         @category = Category.find(params[:id])
         @category.delete
-        render json: {category_id: @category.id }
     end 
 
     private 
+        def set_category
+            @category = Category.find_by_id(params[:id])
+        end 
+
         def category_params
             params.require(:category).permit(:name)
         end
