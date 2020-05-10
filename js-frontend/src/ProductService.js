dbeec820a48a5d67575ef736b01803ab84ad5585
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-05-09 20:01:32 -0400

    Update ProductService.js

--- js-frontend/src/ProductService.js
+++ js-frontend/src/ProductService.js
@@ -1,3 +1,9 @@
 class ProductsService {
-    
+    constructor() { 
+        this.apiURL = 'http://localhost:3000/products'
+    }
+
+    getProducts() {
+        return fetch(this.apiURL).then(response => response.json())
+    }
 }
