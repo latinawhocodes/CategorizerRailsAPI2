382615b58100db6d3fa3e3d134a809d0628d8ad5
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-05-09 20:45:47 -0400

    Update products.js

--- js-frontend/src/components/products.js
+++ js-frontend/src/components/products.js
@@ -1,17 +1,3 @@
 class Products {
-    constructor() {
-        this.products = []
-        this.service = new ProductsService()
-        this.bindEventListeners()
-        this.fetchProducts()
-    }
-
-    fetchProducts() {
-        this.service.getProducts().then(products => {
-        }).then(() => this.displayProducts() ) 
-    }
-
-    displayProducts() {
-        const productContainer = document.getElementById()
-    }
+  
 }
